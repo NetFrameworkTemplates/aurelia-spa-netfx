87b41602af2fa2688ab96745cd216829474742c3
Demis Bellot <demis.bellot@gmail.com>
2017-11-08 04:49:08 -0500

    Update dtos.ts + AssemblyInfo.cs

--- MyApp.ServiceInterface/Properties/AssemblyInfo.cs
+++ MyApp.ServiceInterface/Properties/AssemblyInfo.cs
@@ -10,7 +10,7 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
 [assembly: AssemblyProduct("MyApp.ServiceInterface")]
-[assembly: AssemblyCopyright("Copyright © 2014")]
+[assembly: AssemblyCopyright("Copyright (c) 2017")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
@@ -18,9 +18,6 @@ using System.Runtime.InteropServices;
 // to COM components.  If you need to access a type in this assembly from 
 // COM, set the ComVisible attribute to true on that type.
 [assembly: ComVisible(false)]
-
-// The following GUID is for the ID of the typelib if this project is exposed to COM
-[assembly: Guid("af167918-7e75-47fd-b495-2b04289f359d")]
 
 // Version information for an assembly consists of the following four values:
 //
